5fbc023301ceeff18b5c08c11f04b836dc1bb53d
AnthonyGunardi <anthonygunardi@gmail.com>
2023-10-30 11:40:53 +0700

    disable user verification for upcoming event. Can enable again later

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,10 +6,10 @@ exports.register = async (req,res,next)=>{
           nama_peserta, jabatan_peserta, nama_perusahaan, nomor_hp, skor_tes, persentase_growthMindset, penjelasan_skor
         } = req.body
  
-        const user = await User.findOne({nama_perusahaan, nama_peserta})
-        if (user) {
-            return res.status(401).json({message:"User already exist!"})
-        }
+        // const user = await User.findOne({nama_perusahaan, nama_peserta})
+        // if (user) {
+        //     return res.status(401).json({message:"User already exist!"})
+        // }
         const newUser = await User.create({ nama_peserta, jabatan_peserta, nama_perusahaan, nomor_hp, skor_tes, persentase_growthMindset, penjelasan_skor })
         return res.status(201).json(newUser)
     } catch (err) {
