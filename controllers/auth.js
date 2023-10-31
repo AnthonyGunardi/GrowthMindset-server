09021dcdc6a843783543917f400209b4368574f6
AnthonyGunardi <anthonygunardi@gmail.com>
2023-10-31 09:38:08 +0700

    edit register method on Auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,14 @@ const User = require('../model/user');
 exports.register = async (req,res,next)=>{
     try {
         const {
-          usia, departemen
+          usia, departemen, skor_tes, persentase_growthMindset, penjelasan_skor
         } = req.body
  
         // const user = await User.findOne({nama_perusahaan, nama_peserta})
         // if (user) {
         //     return res.status(401).json({message:"User already exist!"})
         // }
-        const newUser = await User.create({ usia, departemen })
+        const newUser = await User.create({ usia, departemen, skor_tes, persentase_growthMindset, penjelasan_skor })
         return res.status(201).json(newUser)
     } catch (err) {
         return res.status(500).json({message:err.message})
